901c5d85389045e209d58dbf6ac3e4aee199516a
jimyeong <idjjm92@gmail.com>
2021-09-22 01:12:07 +0900

    init

--- src/components/ChatWindow/index.js
+++ src/components/ChatWindow/index.js
@@ -3,7 +3,12 @@ import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { pixelToRem, setColor } from '../../helper';
 import { LayoutContainer } from '../LayoutContainer';
-const ChatWindowBlock = styled.div``;
+const ChatWindowBlock = styled.div`
+  .textarea {
+    resize: none;
+    padding: 0.5rem;
+  }
+`;
 
 function ChatWindow({ match }) {
   const {
@@ -18,9 +23,20 @@ function ChatWindow({ match }) {
   return (
     <ChatWindowBlock>
       <LayoutContainer>
-        <div className="row">
-          <div className="col-7">chating display</div>
-          <div className="col-3">current User List</div>
+        <div className="row ai-s">
+          <div className="col-7">
+            <div className="wrapper col">
+              <ChatDisplay />
+              <textarea className="textarea"></textarea>
+            </div>
+          </div>
+          <div className="col-3">
+            <ChatUser />
+            <ChatUser />
+            <ChatUser />
+            <ChatUser />
+            <ChatUser />
+          </div>
         </div>
       </LayoutContainer>
     </ChatWindowBlock>
@@ -28,3 +44,34 @@ function ChatWindow({ match }) {
 }
 
 export default ChatWindow;
+
+const ChatDisplayBlock = styled.div`
+  height: 700px;
+  background: #b1c5e3;
+`;
+const ChatDisplay = () => {
+  return <ChatDisplayBlock>messages</ChatDisplayBlock>;
+};
+
+/* chat user */
+const ChatUserBlock = styled.div`
+  .row {
+    justify-content: flex-start;
+  }
+`;
+const ChatUser = () => {
+  return (
+    <ChatUserBlock>
+      <div className="row ai-c">
+        <div className="userInfo col">
+          <img src="" alt="userImage" />
+          <div className="login">logout</div>
+        </div>
+        <div className="userDesc">
+          <h3>user Nickname</h3>
+          <p>user email</p>
+        </div>
+      </div>
+    </ChatUserBlock>
+  );
+};
